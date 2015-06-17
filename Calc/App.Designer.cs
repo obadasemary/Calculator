e1713134aefc6dc3ad9fc7d68a238985b4926199
عبدالرحمن السمري <obada.semary@gmail.com>
2015-06-17 13:32:25 +0200

    Calculator 1.0
    base function it is good

---
+++ Calc/App.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Calc
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCalc = new System.Windows.Forms.TextBox();
+            this.txtCalc1 = new System.Windows.Forms.Button();
+            this.txtCalc2 = new System.Windows.Forms.Button();
+            this.txtCalc3 = new System.Windows.Forms.Button();
+            this.txtCalc6 = new System.Windows.Forms.Button();
+            this.txtCalc5 = new System.Windows.Forms.Button();
+            this.txtCalc4 = new System.Windows.Forms.Button();
+            this.txtCalcClear = new System.Windows.Forms.Button();
+            this.txtCalcDot = new System.Windows.Forms.Button();
+            this.txtCalc0 = new System.Windows.Forms.Button();
+            this.txtCalc9 = new System.Windows.Forms.Button();
+            this.txtCalc8 = new System.Windows.Forms.Button();
+            this.txtCalc7 = new System.Windows.Forms.Button();
+            this.txtCalcDev = new System.Windows.Forms.Button();
+            this.txtCalcIn = new System.Windows.Forms.Button();
+            this.txtCalcMuns = new System.Windows.Forms.Button();
+            this.txtCalcPlus = new System.Windows.Forms.Button();
+            this.txtCalcEqual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCalc
+            // 
+            this.txtCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCalc.Location = new System.Drawing.Point(12, 12);
+            this.txtCalc.Name = "txtCalc";
+            this.txtCalc.Size = new System.Drawing.Size(360, 29);
+            this.txtCalc.TabIndex = 0;
+            // 
+            // txtCalc1
+            // 
+            this.txtCalc1.Location = new System.Drawing.Point(12, 53);
+            this.txtCalc1.Name = "txtCalc1";
+            this.txtCalc1.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc1.TabIndex = 1;
+            this.txtCalc1.Text = "1";
+            this.txtCalc1.UseVisualStyleBackColor = true;
+            this.txtCalc1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCalc2
+            // 
+            this.txtCalc2.Location = new System.Drawing.Point(93, 53);
+            this.txtCalc2.Name = "txtCalc2";
+            this.txtCalc2.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc2.TabIndex = 2;
+            this.txtCalc2.Text = "2";
+            this.txtCalc2.UseVisualStyleBackColor = true;
+            this.txtCalc2.Click += new System.EventHandler(this.txtCalc2_Click);
+            // 
+            // txtCalc3
+            // 
+            this.txtCalc3.Location = new System.Drawing.Point(174, 53);
+            this.txtCalc3.Name = "txtCalc3";
+            this.txtCalc3.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc3.TabIndex = 3;
+            this.txtCalc3.Text = "3";
+            this.txtCalc3.UseVisualStyleBackColor = true;
+            this.txtCalc3.Click += new System.EventHandler(this.txtCalc3_Click);
+            // 
+            // txtCalc6
+            // 
+            this.txtCalc6.Location = new System.Drawing.Point(174, 106);
+            this.txtCalc6.Name = "txtCalc6";
+            this.txtCalc6.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc6.TabIndex = 6;
+            this.txtCalc6.Text = "6";
+            this.txtCalc6.UseVisualStyleBackColor = true;
+            this.txtCalc6.Click += new System.EventHandler(this.txtCalc6_Click);
+            // 
+            // txtCalc5
+            // 
+            this.txtCalc5.Location = new System.Drawing.Point(93, 106);
+            this.txtCalc5.Name = "txtCalc5";
+            this.txtCalc5.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc5.TabIndex = 5;
+            this.txtCalc5.Text = "5";
+            this.txtCalc5.UseVisualStyleBackColor = true;
+            this.txtCalc5.Click += new System.EventHandler(this.txtCalc5_Click);
+            // 
+            // txtCalc4
+            // 
+            this.txtCalc4.Location = new System.Drawing.Point(12, 106);
+            this.txtCalc4.Name = "txtCalc4";
+            this.txtCalc4.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc4.TabIndex = 4;
+            this.txtCalc4.Text = "4";
+            this.txtCalc4.UseVisualStyleBackColor = true;
+            this.txtCalc4.Click += new System.EventHandler(this.txtCalc4_Click);
+            // 
+            // txtCalcClear
+            // 
+            this.txtCalcClear.Location = new System.Drawing.Point(174, 212);
+            this.txtCalcClear.Name = "txtCalcClear";
+            this.txtCalcClear.Size = new System.Drawing.Size(75, 47);
+            this.txtCalcClear.TabIndex = 12;
+            this.txtCalcClear.Text = "C";
+            this.txtCalcClear.UseVisualStyleBackColor = true;
+            this.txtCalcClear.Click += new System.EventHandler(this.txtCalcClear_Click);
+            // 
+            // txtCalcDot
+            // 
+            this.txtCalcDot.Location = new System.Drawing.Point(93, 212);
+            this.txtCalcDot.Name = "txtCalcDot";
+            this.txtCalcDot.Size = new System.Drawing.Size(75, 47);
+            this.txtCalcDot.TabIndex = 11;
+            this.txtCalcDot.Text = ".";
+            this.txtCalcDot.UseVisualStyleBackColor = true;
+            this.txtCalcDot.Click += new System.EventHandler(this.txtCalcDot_Click);
+            // 
+            // txtCalc0
+            // 
+            this.txtCalc0.Location = new System.Drawing.Point(12, 212);
+            this.txtCalc0.Name = "txtCalc0";
+            this.txtCalc0.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc0.TabIndex = 10;
+            this.txtCalc0.Text = "0";
+            this.txtCalc0.UseVisualStyleBackColor = true;
+            this.txtCalc0.Click += new System.EventHandler(this.txtCalc0_Click);
+            // 
+            // txtCalc9
+            // 
+            this.txtCalc9.Location = new System.Drawing.Point(174, 159);
+            this.txtCalc9.Name = "txtCalc9";
+            this.txtCalc9.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc9.TabIndex = 9;
+            this.txtCalc9.Text = "9";
+            this.txtCalc9.UseVisualStyleBackColor = true;
+            this.txtCalc9.Click += new System.EventHandler(this.txtCalc9_Click);
+            // 
+            // txtCalc8
+            // 
+            this.txtCalc8.Location = new System.Drawing.Point(93, 159);
+            this.txtCalc8.Name = "txtCalc8";
+            this.txtCalc8.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc8.TabIndex = 8;
+            this.txtCalc8.Text = "8";
+            this.txtCalc8.UseVisualStyleBackColor = true;
+            this.txtCalc8.Click += new System.EventHandler(this.txtCalc8_Click);
+            // 
+            // txtCalc7
+            // 
+            this.txtCalc7.Location = new System.Drawing.Point(12, 159);
+            this.txtCalc7.Name = "txtCalc7";
+            this.txtCalc7.Size = new System.Drawing.Size(75, 47);
+            this.txtCalc7.TabIndex = 7;
+            this.txtCalc7.Text = "7";
+            this.txtCalc7.UseVisualStyleBackColor = true;
+            this.txtCalc7.Click += new System.EventHandler(this.txtCalc7_Click);
+            // 
+            // txtCalcDev
+            // 
+            this.txtCalcDev.Location = new System.Drawing.Point(255, 212);
+            this.txtCalcDev.Name = "txtCalcDev";
+            this.txtCalcDev.Size = new System.Drawing.Size(117, 47);
+            this.txtCalcDev.TabIndex = 16;
+            this.txtCalcDev.Text = "/";
+            this.txtCalcDev.UseVisualStyleBackColor = true;
+            this.txtCalcDev.Click += new System.EventHandler(this.txtCalcDev_Click);
+            // 
+            // txtCalcIn
+            // 
+            this.txtCalcIn.Location = new System.Drawing.Point(255, 159);
+            this.txtCalcIn.Name = "txtCalcIn";
+            this.txtCalcIn.Size = new System.Drawing.Size(117, 47);
+            this.txtCalcIn.TabIndex = 15;
+            this.txtCalcIn.Text = "*";
+            this.txtCalcIn.UseVisualStyleBackColor = true;
+            this.txtCalcIn.Click += new System.EventHandler(this.txtCalcIn_Click);
+            // 
+            // txtCalcMuns
+            // 
+            this.txtCalcMuns.Location = new System.Drawing.Point(255, 106);
+            this.txtCalcMuns.Name = "txtCalcMuns";
+            this.txtCalcMuns.Size = new System.Drawing.Size(117, 47);
+            this.txtCalcMuns.TabIndex = 14;
+            this.txtCalcMuns.Text = "-";
+            this.txtCalcMuns.UseVisualStyleBackColor = true;
+            this.txtCalcMuns.Click += new System.EventHandler(this.txtCalcMuns_Click);
+            // 
+            // txtCalcPlus
+            // 
+            this.txtCalcPlus.Location = new System.Drawing.Point(255, 53);
+            this.txtCalcPlus.Name = "txtCalcPlus";
+            this.txtCalcPlus.Size = new System.Drawing.Size(117, 47);
+            this.txtCalcPlus.TabIndex = 13;
+            this.txtCalcPlus.Text = "+";
+            this.txtCalcPlus.UseVisualStyleBackColor = true;
+            this.txtCalcPlus.Click += new System.EventHandler(this.txtCalcPlus_Click);
+            // 
+            // txtCalcEqual
+            // 
+            this.txtCalcEqual.Location = new System.Drawing.Point(12, 268);
+            this.txtCalcEqual.Name = "txtCalcEqual";
+            this.txtCalcEqual.Size = new System.Drawing.Size(360, 47);
+            this.txtCalcEqual.TabIndex = 17;
+            this.txtCalcEqual.Text = "=";
+            this.txtCalcEqual.UseVisualStyleBackColor = true;
+            this.txtCalcEqual.Click += new System.EventHandler(this.txtCalcEqual_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 327);
+            this.Controls.Add(this.txtCalcEqual);
+            this.Controls.Add(this.txtCalcDev);
+            this.Controls.Add(this.txtCalcIn);
+            this.Controls.Add(this.txtCalcMuns);
+            this.Controls.Add(this.txtCalcPlus);
+            this.Controls.Add(this.txtCalcClear);
+            this.Controls.Add(this.txtCalcDot);
+            this.Controls.Add(this.txtCalc0);
+            this.Controls.Add(this.txtCalc9);
+            this.Controls.Add(this.txtCalc8);
+            this.Controls.Add(this.txtCalc7);
+            this.Controls.Add(this.txtCalc6);
+            this.Controls.Add(this.txtCalc5);
+            this.Controls.Add(this.txtCalc4);
+            this.Controls.Add(this.txtCalc3);
+            this.Controls.Add(this.txtCalc2);
+            this.Controls.Add(this.txtCalc1);
+            this.Controls.Add(this.txtCalc);
+            this.Name = "App";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.App_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCalc;
+        private System.Windows.Forms.Button txtCalc1;
+        private System.Windows.Forms.Button txtCalc2;
+        private System.Windows.Forms.Button txtCalc3;
+        private System.Windows.Forms.Button txtCalc6;
+        private System.Windows.Forms.Button txtCalc5;
+        private System.Windows.Forms.Button txtCalc4;
+        private System.Windows.Forms.Button txtCalcClear;
+        private System.Windows.Forms.Button txtCalcDot;
+        private System.Windows.Forms.Button txtCalc0;
+        private System.Windows.Forms.Button txtCalc9;
+        private System.Windows.Forms.Button txtCalc8;
+        private System.Windows.Forms.Button txtCalc7;
+        private System.Windows.Forms.Button txtCalcDev;
+        private System.Windows.Forms.Button txtCalcIn;
+        private System.Windows.Forms.Button txtCalcMuns;
+        private System.Windows.Forms.Button txtCalcPlus;
+        private System.Windows.Forms.Button txtCalcEqual;
+    }
+}
---
+++ Calc/App.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class App : Form
+    {
+        double number1, number2, result;
+        char op;
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void App_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult RES = MessageBox.Show("Do You Want To Close ? ", "Close", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+            if (RES == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "1";
+        }
+
+        private void txtCalc2_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "2";
+        }
+
+        private void txtCalc3_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "3";
+        }
+
+        private void txtCalc4_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "4";
+        }
+
+        private void txtCalc5_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "5";
+        }
+
+        private void txtCalc6_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "6";
+        }
+
+        private void txtCalc7_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "7";
+        }
+
+        private void txtCalc8_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "8";
+        }
+
+        private void txtCalc9_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "9";
+        }
+
+        private void txtCalc0_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += "0";
+        }
+
+        private void txtCalcDot_Click(object sender, EventArgs e)
+        {
+            txtCalc.Text += ".";
+        }
+
+        private void txtCalcClear_Click(object sender, EventArgs e)
+        {
+            txtCalc.Clear();
+        }
+
+        private void txtCalcPlus_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(txtCalc.Text);
+            op = '+';
+            txtCalc.Clear();
+            txtCalc.Focus();
+        }
+
+        private void txtCalcMuns_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(txtCalc.Text);
+            op = '-';
+            txtCalc.Clear();
+            txtCalc.Focus();
+        }
+
+        private void txtCalcIn_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(txtCalc.Text);
+            op = '*';
+            txtCalc.Clear();
+            txtCalc.Focus();
+        }
+
+        private void txtCalcDev_Click(object sender, EventArgs e)
+        {
+            number1 = Convert.ToDouble(txtCalc.Text);
+            op = '/';
+            txtCalc.Clear();
+            txtCalc.Focus();
+        }
+
+        private void txtCalcEqual_Click(object sender, EventArgs e)
+        {
+            number2 = Convert.ToDouble(txtCalc.Text);
+            switch (op)
+            {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+                case '/':
+                    result = number1 / number2;
+                    break;
+            }
+            txtCalc.Text = result.ToString();
+        }
+    }
+}
